68229dfd9b1ed343b0d410857f54b82b453e7b0f
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2025-09-18 11:48:47 -0300

    developing nav component

--- WebPortfolio/src/components/nav.jsx
+++ WebPortfolio/src/components/nav.jsx
@@ -2,7 +2,18 @@
 
 const nav = () => {
   return (
-    <div>nav</div>
+    <>
+        <div className="flex items-center justify-between  p-10 lg:flex-row">
+            <div>
+                <a href="#" className="text-white font-mono text-3xl tracking-wider flex items-center">AUCODE</a>
+            </div>
+            <div>
+                <a href="#home">Skills</a>
+                <a href="#about">Projects</a>
+                <a href="#services">Testimonials</a>
+            </div>
+        </div>
+    </>
   )
 }
 
