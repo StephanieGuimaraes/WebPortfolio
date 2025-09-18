924df5eb38ae66b477a5fc4ef0fb8b73de8eee82
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2025-09-18 13:21:30 -0300

    icons bib and nav routes

--- WebPortfolio/src/components/nav.jsx
+++ WebPortfolio/src/components/nav.jsx
@@ -1,16 +1,16 @@
-
+import {CgNametag} from 'react-icons/cg';
 
 const nav = () => {
   return (
     <>
         <div className="flex items-center justify-between  p-10 lg:flex-row">
             <div>
-                <a href="#" className="text-white font-mono text-3xl tracking-wider flex items-center">AUCODE</a>
+                <a href="#" className="text-white font-mono text-3xl tracking-wider flex items-center"><CgNametag />Stephanie</a>
             </div>
-            <div>
-                <a href="#home">Skills</a>
-                <a href="#about">Projects</a>
-                <a href="#services">Testimonials</a>
+            <div className="space-x-4">
+                <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Skills</a>
+                <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Projects</a>
+                <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Testimonials</a>
             </div>
         </div>
     </>
