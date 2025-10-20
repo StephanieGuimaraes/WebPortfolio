492f0b10726752e5bc9207b36e2ae8c190a5b798
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2025-10-20 12:24:02 -0300

    add Skills page

--- WebPortfolio/src/components/main.jsx
+++ WebPortfolio/src/components/main.jsx
@@ -1,11 +1,13 @@
 import Header from '../pages/LandingPage/Header'
 import Hero from '../pages/LandingPage/Hero'
+import Skills from '../pages/LandingPage/Skills'
 
 const main = () => {
   return (
     <div>
         <Header />
         <Hero />
+        <Skills />
     </div>
   )
 }
