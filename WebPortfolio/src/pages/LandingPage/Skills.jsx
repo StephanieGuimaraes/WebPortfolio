127599fd23fbd314ac2766264df824602defb143
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2025-10-20 12:24:15 -0300

    creation of the Skills page

---
+++ WebPortfolio/src/pages/LandingPage/Skills.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+const softwareSkills = [
+    {
+        name: 'JavaScript',
+        level: 'Advanced',
+    },
+    {
+        name: 'TypeScript',
+        level: 'Advanced',
+    },
+    {
+        name: 'React',
+        level: 'Advanced',
+    },
+    {
+        name: 'Tailwind CSS',
+        level: 'Intermediate',
+    },
+    {
+        name: 'CSS',
+        level: 'Advanced',
+    },
+    {
+        name: 'HTML',
+        level: 'Advanced',
+    },
+    {
+        name: 'Api Rest',
+        level: 'Intermediate',
+    },
+    {
+        name: 'Api integration',
+        level: 'Intermediate',
+    },
+    {
+        name: 'Node.js',
+        level: 'Beginner',
+    },
+    {
+        name: 'Figma',
+        level: 'Intermediate',
+    },
+    {
+        name: 'Git',
+        level: 'Advanced',
+    },
+    {
+        name: 'GitHub',
+        level: 'Advanced',
+    },
+    {
+        name: 'GitActions',
+        level: 'Beginner',
+    },
+    {
+        name: 'Aggrid',
+        level: 'Beginner',
+    },
+    {
+        name: 'Cypress',
+        level: 'Beginner',
+    },
+    {
+        name: 'C++',
+        level: 'Intermediate',
+    },
+    {
+        name: 'C#',
+        level: 'Beginner',
+    }
+]
+
+const engineeringSkills = []
+
+const Skills = () => {
+  return (
+    <section id='skills' className='scroll-my-20 py-24 bg-slate-50'>
+        <div className='container mx-auto px-4'>
+            <div className='text-center mb-16'>
+                <h2 className='text-4xl font-bold text-indigo-600 mb-4'>Skills</h2>
+                <p className='text-slate-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Skills
