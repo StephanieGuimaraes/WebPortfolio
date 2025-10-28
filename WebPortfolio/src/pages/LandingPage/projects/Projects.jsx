21df55f05ed7f76310c363c33f970739e7261f8f
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2025-10-28 19:37:55 -0300

    change project place

---
+++ WebPortfolio/src/pages/LandingPage/projects/Projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <section id='projects' className='scroll-my-20 py-24 bg-slate-50 lg:px-64'>
+        <div className='w-full mx-auto'>
+            <div className='text-center mb-16'>
+                <h2 className='text-4xl font-bold text-indigo-600 mb-4'>Projects</h2>
+                <p className='text-slate-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Projects
