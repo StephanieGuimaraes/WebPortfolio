c119630295c01afbc6439812a05273e354a94b6e
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2025-10-29 16:14:26 -0300

    content cards

--- WebPortfolio/src/pages/LandingPage/projects/Projects.jsx
+++ WebPortfolio/src/pages/LandingPage/projects/Projects.jsx
@@ -6,51 +6,51 @@ const Projects = () => {
 
   const Cards = [
     {
-      title: "Project 1",
+      title: "Pilot",
       subTitle: "Subtitle 1",
       content: "This is the content for project 1.",
       image: <img src={ProfilePic} alt='Profile' className='w-80 lg:w-[350px] rounded-4xl pr-4 sm:pr-6 lg:pr-12'></img>
     },
     {
-      title: "Project 2",
+      title: "RV Software",
       subTitle: "Subtitle 2",
       content: "This is the content for project 2.",
       image: <img src={ProfilePic} alt='Profile' className='w-80 lg:w-[350px] rounded-4xl pr-4 sm:pr-6 lg:pr-12'></img>
     },
     {
-      title: "Project 3",
+      title: "AQtech",
       subTitle: "Subtitle 3",
       content: "This is the content for project 3.",
       image: <img src={ProfilePic} alt='Profile' className='w-80 lg:w-[350px] rounded-4xl pr-4 sm:pr-6 lg:pr-12'></img> 
     },
     {
-      title: "Project 4",
-      subTitle: "Subtitle 4",
-      content: "This is the content for project 4.",
+      title: "Fluxmed",
+      subTitle: "Manage your clinic with Fluxmed.",
+      content: "Fluxmed is a comprehensive solution for clinic management, offering tools for patient scheduling, electronic health records, Teleservice and billing.",
       image: <img src={ProfilePic} alt='Profile' className='w-80 lg:w-[350px] rounded-4xl pr-4 sm:pr-6 lg:pr-12'></img>
     },
     {
-      title: "Project 5",
-      subTitle: "Subtitle 5",
-      content: "This is the content for project 5.",
+      title: "AGHUx",
+      subTitle: "Management Application for University Hospitals",
+      content: "The goal of the application is to standardize the care and administrative practices of Federal University Hospitals and to allow the creation of standardized indicators, which will facilitate the adoption of common improvement projects for these hospitals.",
       image: <img src={ProfilePic} alt='Profile' className='w-80 lg:w-[350px] rounded-4xl pr-4 sm:pr-6 lg:pr-12'></img> 
     },
     {
-      title: "Project 6",
-      subTitle: "Subtitle 6",
-      content: "This is the content for project 6.",
+      title: "CLARA Beep Solutions",
+      subTitle: "Technology and innovation in laparoscopic surgery",
+      content: "CLARA is a tool to assist in performing laparoscopy procedures. The system provides greater precision and camera stability for doctors during the surgical procedure through an optical positioning control system that is easily adjustable via joystick or foot pedal.",
       image: <img src={ProfilePic} alt='Profile' className='w-80 lg:w-[350px] rounded-4xl pr-4 sm:pr-6 lg:pr-12'></img>
     },
     {
-      title: "Project 7",
+      title: "Kuaray",
       subTitle: "Subtitle 7",
       content: "This is the content for project 7.",
       image: <img src={ProfilePic} alt='Profile' className='w-80 lg:w-[350px] rounded-4xl pr-4 sm:pr-6 lg:pr-12'></img>
     },
     {
-      title: "Project 8",
-      subTitle: "Subtitle 8",
-      content: "This is the content for project 8.",
+      title: "LaicanSat",
+      subTitle: "Mockup with CubeSat Nanosatellite pattern.",
+      content: "LaicanSat is a CubeSat 2U nanosatellite platform. It features an onboard computer, tracking system, and 360-degree cameras. Its purpose is to test new materials in space environments and to explore new mechanical designs, electronics, and sensors for aerospace research.",
       image: <img src={ProfilePic} alt='Profile' className='w-80 lg:w-[350px] rounded-4xl pr-4 sm:pr-6 lg:pr-12'></img>
     },
   ]
