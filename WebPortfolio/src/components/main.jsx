6353be97ba2b58ad78dd22459fc4dff6e09e0fe8
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2025-10-29 13:28:52 -0300

    fix projects path

--- WebPortfolio/src/components/main.jsx
+++ WebPortfolio/src/components/main.jsx
@@ -1,6 +1,6 @@
 import Header from '../pages/LandingPage/Header'
 import Hero from '../pages/LandingPage/Hero'
-import Projects from '../pages/LandingPage/Projects'
+import Projects from '../pages/LandingPage/projects/Projects'
 import Skills from '../pages/LandingPage/Skills'
 
 const main = () => {
